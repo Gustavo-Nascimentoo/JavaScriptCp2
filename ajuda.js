4e809340f0c88fc44c376808de1618d660298c27
Gustavo-Nascimentoo <gustavoalm1202@gmail.com>
2026-05-13 19:57:12 -0300

    fix/ajuda

---
+++ ajuda.js
@@ -0,0 +1,71 @@
+// let senha;
+
+// do{
+//     senha = prompt("Digite sua senha:");
+//     if(!senha || senha.length < 8){
+//         alert("Digite uma senha válida");
+//     }
+// } while(!senha || senha.length < 8);
+
+// alert (`Login efetuado, aproveite ${nome}`);
+// console.log(nome, senha);
+
+// if(n1 === "" || n2 === "" || operacao === ""){
+//     alert(`Digite alguma coisa`);
+// }
+// if(n1 === null || n2 === null || operacao === null){
+//     alert(`Precisa de alguma coisa`);
+// }
+// if(isNaN(n1) || isNaN(n2)){
+//     alert(`Digite direito`);
+
+// const numero = Number(prompt("Digite um número:"))
+
+// if (isNaN(numero) || numero <= 0) {
+//     alert("Digite um número válido maior que zero!")
+// } else {
+//     let divisores = ""
+
+//     for (let i = 1; i <= numero; i++) {
+//         if (numero % i === 0) {
+//             divisores = divisores + `${i}\n`
+//         }
+//     }
+
+//     alert(`Divisores de ${numero}:\n\n${divisores}`)
+// }
+
+//função conta primo
+ 
+// function verificarPrimo(numero){
+//     if(numero < 2){
+//         return "Não é primo"
+//     }
+//     for(let i = 2; i < numero; i++){
+//         if(numero % i === 0){
+//             return "Não é primo"
+//         }
+//     }
+//     return "É primo"
+// }
+ 
+// let n = Number(prompt("Digite um número:"))
+ 
+// alert(verificarPrimo(n))
+
+// trim() remove espaços em branco
+//console.log(nome.length)
+//login.toLowerCase().includes("@"))
+// ! "significa não"
+// console.log(nome.replace("a", "o"))
+//numero1.tofixed(2)
+//isNaN(valor)
+// alert(`Seu imc é ${imc.toFixed(2)}`);
+
+//Math.random() aleatorio de 0 à 0.99999
+//Math.floor(Math.floor() * 7) para arrendondar para inteiro "+1" caso queira que start no 1
+//se vc quiser de 0 a 6 Math.random coloque 1 a cima do normal Math.random * 7
+//Math.floor() arredonda pra baixo
+//Math.ceil() arredonda pra cima
+//Math.round() arredonda
+
